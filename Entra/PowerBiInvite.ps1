a1057cf36919915f1c229e2bfdc23e705f063145
tschoolar <tom.schoolar@cps.gov.uk>
2026-02-20 09:33:24 +0000

    Update PowerBiInvite.ps1

--- Entra/PowerBiInvite.ps1
+++ Entra/PowerBiInvite.ps1
@@ -1,5 +1,3 @@
-$token = (az account get-access-token --resource https://graph.microsoft.com --query accessToken -o tsv)
-$secureToken = $token | ConvertTo-SecureString -AsPlainText -Force                                      
-Connect-MgGraph -AccessToken $secureToken
+Connect-MgGraph
 
 New-MgInvitation -InvitedUserDisplayName "Name" -InvitedUserEmailAddress "name@herts.police.uk" -InviteRedirectUrl "https://myapplications.microsoft.com" -SendInvitationMessage:$true
