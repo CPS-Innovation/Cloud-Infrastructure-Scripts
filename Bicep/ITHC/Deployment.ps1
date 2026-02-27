b1431362fbc7c64d7b01c982e3b6935fde4d266f
tschoolar <tom.schoolar@cps.gov.uk>
2026-02-27 16:24:31 +0000

    Update Deployment.ps1

--- Bicep/ITHC/Deployment.ps1
+++ Bicep/ITHC/Deployment.ps1
@@ -1,7 +1,7 @@
 # New Network Variables    
 $projectName = "Tom"
 $location = "UKSouth"   # "uksouth" or "ukwest"
-$newVnetAddress = "10.0.0.0/16"      
+$newVnetAddress = "10.1.1.0/24"      
 
 
 # Hub Network Variables
@@ -13,7 +13,6 @@ $hubVnetName = "uk$locationInital-vnet-vft01"
 $hubResourceGroupName = "uk$locationInital-rg-vft01"   
 $hubSubscriptionName = "ExpressRoute CLZ Hub (Prod)"
 $dnsServers = @("10.7.136.4", "10.14.136.4")
-$firewallAddress = "10.8.0.4"
 
 # Static Variables
 $subscriptionName = "Cloud Sandbox"  
@@ -22,16 +21,25 @@ $upperCaseInital = $locationInital.ToString().ToUpper()
 $endBitOfName = "-C$upperCaseInital" + "L-ITHC-$projectName"
 
 $routeTableName = "RT$endBitOfName"
-$nsgName = "NSG$endBitOfName"
 $defaultRouteName = "default"
 $newVnetName = "VNET$endBitOfName"
+$pipName = "PIP$endBitOfName"
+$nicName = "NIC$endBitOfName"
 
 $resourceGroupName = "RG$endBitOfName"
 
 $hubToSpoke = "Hub-to-$newVnetName"
 $spokeToHub = "$newVnetName-to-Hub"
-$everything = "0.0.0.0/0"
 
+#DNS Servers
+$dnsServers = '10.7.136.4','10.14.136.4'
+$firewallAddress = "10.8.0.4"
+
+if($locationInital -eq "w")
+{
+    $firewallAddress = "10.8.16.4" #CHECK
+    $dnsServers = '10.14.136.4','10.7.136.4' #IMPROVE
+}
 
 $subscription = Get-AzSubscription -SubscriptionName $subscriptionName
 $subscriptionId = $subscription.Id
@@ -44,7 +52,15 @@ Set-AzContext $subscriptionId
 
 New-AzSubscriptionDeployment -Location "uksouth" -TemplateFile "C:\Temp\BicepTest.bicep" -rgName $resourceGroupName
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "C:\Temp\BicepTest.bicep" -Mode Incremental #Complete
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "C:\Temp\BicepTest2.bicep" `
+    -vnetName $newVnetName `
+    -routeTableName $routeTableName `
+    -pipName $pipName `
+    -firewallAddress $firewallAddress `
+    -dnsServers $dnsServers `
+    -nicName $nicName `
+    -vnetAddressRange $newVnetAddress `
+    -Mode Incremental #Complete
 
 
 
